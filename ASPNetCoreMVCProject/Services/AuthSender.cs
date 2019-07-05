b8b33d2db3922220f95c70d9245ab67244a0eda2
IAmNC <NickyChau95@gmail.com>
2019-07-05 16:16:10 +0100

    Added AuthSender to fetch SendGrid API Key

---
+++ ASPNetCoreMVCProject/Services/AuthSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMVCProject.Services
+{
+    public class AuthSender
+    {
+        //Values stored as environment variables 
+        public string SendGridUser { get; set; }
+        //API key - Needed for emails to be sent by SendGrid
+        public string SendGridKey { get; set; }
+    }
+}
