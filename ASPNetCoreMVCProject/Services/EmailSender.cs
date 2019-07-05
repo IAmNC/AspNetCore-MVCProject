595ff168692b604423fbd124bce1677b957336f3
IAmNC <NickyChau95@gmail.com>
2019-07-05 16:18:12 +0100

    Updated Email Sender class to use SendGrid

--- ASPNetCoreMVCProject/Services/EmailSender.cs
+++ ASPNetCoreMVCProject/Services/EmailSender.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 
 namespace ASPNetCoreMVCProject.Services
 {
-    // This class is used by the application to send email for account confirmation and password reset.
-    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
+        public EmailSender(IOptions<AuthSender> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthSender Options { get; } //Environment Variables
+
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Task.CompletedTask;
+            return Execute(Options.SendGridKey, subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("MVCProject@email.com", "Nicky Chau"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
         }
     }
 }
